c66cbf822f5bd25f0434765e24772e81960281fb
Babkou Andrei <babkou.a@profitero.com>
2020-10-19 20:15:07 +0300

    fix typo

--- spec/api/controllers/cors_headers_spec.rb
+++ spec/api/controllers/cors_headers_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Api::Controllers::Authentication, type: :action do
+RSpec.describe Api::Controllers::CorsHeaders, type: :action do
   let(:action) { Api::Controllers::Users::Show.new }
   let(:params) { Hash[] }
   let(:repository) { UserRepository.new }
